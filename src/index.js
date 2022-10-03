817257e678c7048b060a601311b4ba2407037e9c
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-10-03 21:41:55 +0300

    add start files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { App } from 'components/ContactsBook/App';
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        {/* <BrowserRouter basename="/goit-react-hw-08-phonebook/"> */}
         <App />
+        {/* </BrowserRouter> */}
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
